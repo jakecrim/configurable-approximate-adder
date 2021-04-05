c784b8fcdcf06fa6ed2853402e4a7459b2f23a6d
YoungBrendanM <54420175+YoungBrendanM@users.noreply.github.com>
2021-04-05 09:10:27 -0500

    Carry Predict completed

---
+++ CarryPredict.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity CarryPredict is
+port(A, B: in std_logic_vector(1 downto 0);;  
+    CPredict, BP: out std_logic);
+
+end CarryPredict;
+
+architecture behavior of CarryPredict is
+
+begin
+    
+    CPredict <= (A(1) and B(1)) or (A(0) and B(0) and (A(1) xor B(1)));
+    BP < (A(1) xor B(1)) and (A(0) xor B(0));
+
+end behavior;
