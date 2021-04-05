3d4a9bb162da9282f93d0b1082a633afbee4e0ff
Jacob Higgins <higginsjake909@gmail.com>
2021-04-05 13:40:56 -0500

    udates

--- adder-2.vhd
+++ adder-2.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity adder2 is
-    port(P,G: in std_logic_vector(1 downto 0);
+    port(A,B: in std_logic_vector(1 downto 0);
     Ci: in std_logic;
     S: out std_logic_vector(1 downto 0);
     Co: out std_logic);
@@ -15,16 +15,11 @@ Architecture Structure of adder2 is
         Cout, Sum: out std_logic);
     end component;
 
-    -- *NOT SURE WHICH*
-    -- signal C: std_logic_vector(1 downto 1);
     signal C: std_logic;
     
     begin
-        -- *NOT SURE WHICH*
-        -- FA1: FullAdder port map (P(0), G(0), Ci, C(1), S(0));
-        -- FA2: FullAdder port map (P(1), G(1), C(1), Co, S(1));
-        FA1: FullAdder port map (P(0), G(0), Ci, C, S(0));
-        FA2: FullAdder port map (P(1), G(1), C, Co, S(1));
+        FA1: FullAdder port map (A(0), B(0), Ci, C, S(0));
+        FA2: FullAdder port map (A(1), B(1), C, Co, S(1));
 
 
 end Structure;
