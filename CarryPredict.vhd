e56f4cb4e61dcf4041c360535a14dac690ffdd40
YoungBrendanM <54420175+YoungBrendanM@users.noreply.github.com>
2021-04-07 21:46:07 +0000

    Update CarryPredict.vhd

--- CarryPredict.vhd
+++ CarryPredict.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity CarryPredict is
-port(A, B: in std_logic_vector(1 downto 0);;  
+port(A, B: in std_logic_vector(1 downto 0);  
     CPredict, BP: out std_logic);
 
 end CarryPredict;
@@ -12,6 +12,6 @@ architecture behavior of CarryPredict is
 begin
     
     CPredict <= (A(1) and B(1)) or (A(0) and B(0) and (A(1) xor B(1)));
-    BP < (A(1) xor B(1)) and (A(0) xor B(0));
+    BP <= (A(1) xor B(1)) and (A(0) xor B(0));
 
 end behavior;
